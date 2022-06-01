55ca5b390507c7f0e10ed27d2f4dad772276a394
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-06-01 11:47:28 +0600

    create battery entity

---
+++ src/main/java/com/example/demo/entities/Battery.java
@@ -0,0 +1,34 @@
+/**
+ * Created By: Md. Nazmus Salahin
+ * Created Date: 1-Jun-2022
+ * Time: 11:44 PM
+ * Modified By:
+ * Modified date:
+ * (C) CopyRight Salahin ltd.
+ */
+
+package com.example.demo.entities;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "battery")
+public class Battery {
+	
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "uuid2")
+	@Column(name = "id", nullable = false)
+	private UUID id;
+	
+	@Column(name="postcode")
+	private Integer postcode;
+	
+	@Column(name="watt")
+	private Integer watt;
+}
